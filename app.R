2e6599c884e6732d88eb0ad4b8d36170c260bea9
willking98 <64603291+willking98@users.noreply.github.com>
2020-05-28 23:12:58 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,168 @@
+library(shiny)
+library(rvest)
+library(haven)
+library(stringr)
+library(tidyverse)
+library(fuzzyjoin)
+library(stringdist)
+options(warn = 0)
+
+ui <- fluidPage(
+    titlePanel('Download the British National Formulary'),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("dataset", "Choose a dataset:", 
+                        choices = c("BNF", "BNF_min")),
+            radioButtons("filetype", "File type:",
+                         choices = c("csv")),
+            downloadButton('downloadData', 'Download')
+        ),
+        mainPanel(
+        
+        )
+    )
+)
+
+server <- function(input, output) {
+    
+    ### INPUT WEBSCRAPE CODE
+    
+    drug_list_url <- "https://bnf.nice.org.uk/drug/"
+    drugpage <- read_html(drug_list_url)
+    drugs <- html_text(html_nodes(drugpage, 'span'))
+    drugs <- drugs[3:length(drugs)]
+    drugs <- tolower(drugs)
+    
+    
+    #setwd("/Users/Will/Desktop/Work from home/ALTAR/Trial")
+    #drugs <- read.csv("BNF Drugs.csv")
+    #drugs <- drugs$name
+    #drugs <- tolower(drugs)
+    drugs <- str_replace_all(drugs, " ", "-")
+    drugs <- str_replace_all(drugs, ",", "")
+    drugs <- str_replace_all(drugs, "é", "e")
+    drugs <- str_replace(drugs, "\\(", "")
+    drugs <- str_replace(drugs, "\\)", "")
+    drugs <- str_replace(drugs, "'", "")
+    
+    drugs <- str_replace_all(drugs, "d-(rh0)-", "d-rh0-")
+    drugs <- sub("noradrenaline/norepinephrine", "noradrenalinenorepinephrine", drugs)
+    drugs <- str_replace(drugs, "enaline/epinephr", "enalineepinephr")
+    drugs <- sub("-$", "", drugs)
+    drugs <- drugs[-c(87, 134:137, 163, 216, 236, 1628, 1629)] 
+    url <- "https://bnf.nice.org.uk/medicinal-forms/anastrozole.html"
+    webpage <- read_html(url)
+    
+    #Using CSS selectors to scrape the desired information
+    name <- html_nodes(webpage,'span.strengthOfActiveIngredient')
+    size <- html_nodes(webpage, 'td.packSize')
+    price <- html_nodes(webpage, 'td.nhsIndicativePrice')
+    provider <- html_nodes(webpage, 'span.manufacturer')
+    #Converting the ranking data to text
+    name <- html_text(name)
+    size <- html_text(size)
+    price <- html_text(price)
+    
+    # Check the extraction is works
+    
+    name1 <- unlist(strsplit(name, "<"))
+    price <- unlist(strsplit(price, "\n                            "))
+    price1 <- price[seq(2, length(price), 3)]
+    
+    z <- cbind(name1, size, price1)
+    
+    
+    
+    for(i in drugs){
+        print(i)
+        url <- paste("https://bnf.nice.org.uk/medicinal-forms/", i, ".html", sep = "")
+        webpage <- read_html(url)
+        
+        #Using CSS selectors to scrape the desired information
+        name <- html_nodes(webpage,'span.strengthOfActiveIngredient')
+        size <- html_nodes(webpage, 'td.packSize')
+        price <- html_nodes(webpage, 'td.nhsIndicativePrice')
+        
+        
+        #Converting the ranking data to text
+        name1 <- html_text(name)
+        size1 <- html_text(size)
+        price1 <- html_text(price)
+        
+        if(length(name)!=0){
+            
+            name1 <- unlist(strsplit(name1, "<"))
+            price1 <- unlist(strsplit(price1, "\n                            "))
+            price1 <- price1[seq(2, length(price1), 3)]
+            
+            
+            z1 <- cbind(name1, size1, price1)
+            z <- rbind(z, z1)
+        }
+    }
+    
+    # Choosing the lowest price for every given active ingredient
+    
+    BNF <- as.data.frame(z)
+    BNF <- BNF %>% 
+        rename(ActiveIngredients = "name1", Size = "size", Price = "price1") %>% 
+        mutate(Price = as.character(Price)) %>% 
+        mutate(Price = str_replace(Price, "£", "")) %>%
+        mutate(Price = as.numeric(Price)) %>% 
+        mutate(ActiveIngredients = as.character(ActiveIngredients)) %>% 
+        select(ActiveIngredients, Size, Price)
+    
+    code <- seq(1:length(BNF$ActiveIngredients))
+    code <- paste("A", code, sep = "")  
+    BNF <- BNF %>% 
+        add_column(Code = code, .before = "ActiveIngredients")
+    
+    
+    BNF_min <- BNF %>% 
+        group_by(ActiveIngredients) %>% 
+        slice(which.min(Price)) 
+    
+    
+    
+    
+    ### SHINY CONTINUE
+    
+    
+    datasetInput <- reactive({
+        # Fetch the appropriate data object, depending on the value
+        # of input$dataset.
+        switch(input$dataset,
+               "BNF" = BNF,
+               "BNF_min" = BNF_min)
+    })
+    
+    output$table <- renderTable({
+        datasetInput()
+    })
+    
+    # downloadHandler() takes two arguments, both functions.
+    # The content function is passed a filename as an argument, and
+    #   it should write out data to that filename.
+    output$downloadData <- downloadHandler(
+        
+        # This function returns a string which tells the client
+        # browser what name to use when saving the file.
+        filename = function() {
+            paste(input$dataset, input$filetype, sep = ".")
+        },
+        
+        # This function should write data to a file given to it by
+        # the argument 'file'.
+        content = function(file) {
+            sep <- switch(input$filetype, "csv" = ",", "tsv" = "\t")
+            
+            # Write to a file specified by the 'file' argument
+            write.table(datasetInput(), file, sep = sep,
+                        row.names = FALSE)
+        }
+    )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
