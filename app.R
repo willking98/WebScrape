888e8e1eecc5a7977569aefa9b8bc9877ab69af5
willking98 <64603291+willking98@users.noreply.github.com>
2020-05-30 13:35:28 +0100

    Update app.R

--- app.R
+++ app.R
@@ -93,7 +93,7 @@ code <- seq(1:length(BNF$ActiveIngredients))
 code <- paste("A", code, sep = "")
 BNF <- BNF %>%
     add_column(Code = code, .before = "ActiveIngredients") %>%
-    mutate(Dose = parse_number(ActiveIngredients)) %>%
+    mutate(Dose = parse_number(ActiveIngredients))
 
 
 
